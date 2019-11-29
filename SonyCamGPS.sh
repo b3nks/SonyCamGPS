936dc3321c8b322fbe6209ba4e77582aff439f76
b3nks <github@benks.io>
2019-11-29 16:56:35 +0200

    Add Script

---
+++ SonyCamGPS.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Download GPS Help Data for your Sony Alpha 77
+# put this script in the root directory of your SD Card of your Sony camera an run it.
+# The old data file will be renamed. An old backup will be overwritten.
+
+BASEDIR=$(dirname $0)
+
+mkdir -p $BASEDIR/PRIVATE/SONY/GPS
+cd $BASEDIR/PRIVATE/SONY/GPS
+mv assistme.dat assistme.dat_OLD
+wget http://control.d-imaging.sony.co.jp/GPS/assistme.dat
+
